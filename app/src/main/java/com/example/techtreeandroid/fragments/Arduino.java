d84f7e7dcd8f433af0b6eaf09e573c44ee5cebe4
sarim <sarimk80>
2019-07-02 14:14:04 +0500

    New fragments and minor change in UI

--- app/src/main/java/com/example/techtreeandroid/fragments/Arduino.java
+++ app/src/main/java/com/example/techtreeandroid/fragments/Arduino.java
@@ -3,30 +3,55 @@ package com.example.techtreeandroid.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.techtreeandroid.R;
+import com.example.techtreeandroid.databinding.FragmentArduinoBinding;
+import com.example.techtreeandroid.fragmentevent.ArduinoEvent;
+import com.example.techtreeandroid.fragmentmodel.ArduinoModel;
 
 
 public class Arduino extends Fragment {
-
+    private FragmentArduinoBinding arduinoBinding;
 
     public Arduino() {
         // Required empty public constructor
     }
 
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_arduino, container, false);
+        arduinoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_arduino, container, false);
+        return arduinoBinding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        arduinoBinding.setModel(new ArduinoModel("Arduino", "Product"));
 
+        arduinoBinding.setEvent(new ArduinoEvent() {
+            @Override
+            public void ControllerEvent() {
+                Navigation.findNavController(arduinoBinding.getRoot()).navigate(R.id.action_arduino_to_ard_Detail);
+            }
+
+            @Override
+            public void ProductEvent() {
+                Navigation.findNavController(arduinoBinding.getRoot()).navigate(R.id.action_arduino_to_prod_Deatil);
+            }
+        });
+    }
 }
